75925c254657994f7321032152aedbe204a9694c
Eliasonetor <ilya_mazko@epam.com>
2021-11-14 20:25:25 +0300

    Excel parse new

--- test.js
+++ test.js
@@ -1,1 +1,14 @@
-console.log('Hello');
+const axios = require("axios");
+const XLSX = require("xlsx");
+const { default: async } = require("async");
+
+var filePath = "./mass_upload_locations.xlsx";
+
+module.exports.index = async function () {
+    var workbook = XLSX.readFile(filePath);
+    var sheetNameList = workbook.SheetNames;
+    var data = XLSX.utils.sheet_to_json(workbook.Sheets[sheetNameList[0]]);
+    console.log(data);
+}
+
+this.index();
